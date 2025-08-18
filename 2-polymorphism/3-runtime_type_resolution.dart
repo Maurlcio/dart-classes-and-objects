caf36f4b4edeec9aeb4acf521bbf2985b7acea8f
Maurlcio <thecoolermess@gmail.com>
2025-08-17 21:08:49 -0300

    batch of polymorphism Dart exercises

---
+++ 2-polymorphism/3-runtime_type_resolution.dart
@@ -0,0 +1,30 @@
+abstract class Animal {
+  makeSound();
+}
+
+class Dog implements Animal{
+  @override
+  makeSound() {
+    print("Woof!");
+  }
+}
+
+class Cat implements Animal{
+  @override
+  makeSound(){
+    print("Meow!");
+    print("```****");
+  }
+}
+
+void main() {
+  List<Animal> pets = [Dog(), Cat()];
+
+  for (var pet in pets) {
+    print(pet.runtimeType);
+    pet.makeSound();
+    if (pet is Dog) {
+      print("It's a dog");
+    }
+  }
+}
